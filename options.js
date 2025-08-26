1be73fe9289d314259281b333e3fde8bb5169e86
Franz04Rony <ventocilla2p@hotmail.com>
2025-08-26 07:18:52 -0500

    updated options

--- options.js
+++ options.js
@@ -2,8 +2,8 @@ export const options = {
     "Ver animes desde la cama": "#8E3E63",
     "Jugar Autochess": "#D2649A",
     "Ver videos de Youtube": "#F6FAB9",
-    "Actualizar foto en LinkedIn": "#CAE6B2",
-    "red": "#e63946",
-    "honey": "#f1faee",
-    "ultra violento we": "#52489c",
+    "Acariciar a mi gato": "#CAE6B2",
+    "Desinstalar Bumble xd": "#e63946",
+    "Ir de compras a Metro": "#f1faee",
+    "Aprender Rust (enserio)": "#52489c",
 }
